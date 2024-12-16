1912a066321edfaa159bb7b5f5909fd2c5421584
me <strelokask@gmail.com>
2024-12-16 08:58:39 +0500

    addded task-cancel sample

---
+++ task/TasksLab/TasksLab/TaskCancel.cs
@@ -0,0 +1,37 @@
+﻿namespace TasksLab;
+
+public class TaskCancel
+{
+    public static async Task Run()
+    {
+        var cts = new CancellationTokenSource();
+        var task = Task.Run(() => ProcessCts(cancellationToken: cts.Token));
+    
+        Task.Delay(2000).GetAwaiter().GetResult();
+        cts.Cancel();
+        try
+        {
+            await task;
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Canceled");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Failed");
+        }
+    }
+
+
+    static async Task ProcessCts(CancellationToken cancellationToken)
+    {
+
+        for (var i = 0;; i++)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            Console.WriteLine($"work {i}");
+            await Task.Delay(TimeSpan.FromSeconds(0.5), cancellationToken);
+        }
+    }
+}
