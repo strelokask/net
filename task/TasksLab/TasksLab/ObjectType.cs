da89c71a5760e2a02af17fab9d3ca44a415b3a71
me <strelokask@gmail.com>
2024-12-16 09:41:06 +0500

    compare value&reference types

---
+++ task/TasksLab/TasksLab/ObjectType.cs
@@ -0,0 +1,13 @@
+﻿namespace TasksLab;
+
+public class ObjectType
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public struct ValueType
+{
+    public ObjectType ObjectType { get; set; }
+    public string StringType { get; set; }
+}
--- task/TasksLab/TasksLab/Program.cs
+++ task/TasksLab/TasksLab/Program.cs
@@ -1,3 +1,26 @@
 ﻿using TasksLab;
+using ValueType = TasksLab.ValueType;
 
-await TaskCancel.Run();
+
+// await TaskCancel.Run();
+
+
+var valueType = new ValueType()
+{
+    StringType = "string",
+    ObjectType = new ObjectType()
+    {
+        Id = 123,
+        Name = "original"
+    }
+};
+
+var copyType = valueType;
+
+valueType.StringType = "updated";
+valueType.ObjectType.Id = 999;
+valueType.ObjectType.Name = "updatedName";
+//StringType изменится, т.к. создается новая строка
+//ObjectType НЕ изменится, т.к. хранит ССЫЛКУ на объект
+Console.WriteLine($"{valueType.StringType} : {valueType.ObjectType.Id}, {valueType.ObjectType.Name}");
+Console.WriteLine($"{copyType.StringType} : {copyType.ObjectType.Id}, {copyType.ObjectType.Name}");
